1468ee40d11aa5e9bb1134869252f11aa414636f
FirminoMassango <firminorafael0@gmail.com>
2022-08-01 10:02:52 +0200

    Modify footer text color and font weight

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@ function Footer() {
   return (
     <div className="text-gray-400 text-center py-4">
       created by{" "}
-      <span className="text-gray-500 font-bold">Firmino Massango</span> -
-      devChallenges.io
+      <span className="text-gray-500 font-bold border-b border-gray-500">
+        Firmino Massango
+      </span>{" "}
+      - devChallenges.io
     </div>
   );
 }
